089226d6aaecca055cad743fedce08d844d6301a
supakit-1 <6631501109@lamduan.mfu.ac.th>
2026-05-31 01:27:33 +0700

    style: update skills section in TechStack.tsx with refined listings

--- app/sections/TechStack.tsx
+++ app/sections/TechStack.tsx
@@ -12,26 +12,23 @@ export default function TechStack() {
           </h2>
         </AnimatedSection>
 
-        <div>
           <ul className="space-y-4 text-slate-500 dark:text-slate-400 list-disc list-outside pl-5 marker:text-black dark:marker:text-white">
             <li>
-              <span className="font-bold text-black dark:text-white mr-1">Networking:</span> OSI Model, TCP/IP, IPv4/IPv6 Subnetting, VLSM, VLAN, Trunking, STP, EtherChannel, Port Security
+              <span className="font-bold text-black dark:text-white mr-1">Networking:</span> OSI Model, TCP/IP, IPv4/IPv6, VLSM Subnetting, VLAN, STP, EtherChannel, Port Security, Static Route, Default Route, OSPF, EIGRP, BGP, DHCP, NAT/PAT, ACL, VPN, SNMP.
             </li>
             <li>
-              <span className="font-bold text-black dark:text-white mr-1">Routing Protocols:</span> Static Route, Default Route, EIGRP, OSPF, BGP
+              <span className="font-bold text-black dark:text-white mr-1">Tools & Technologies:</span> Cisco Packet Tracer, GNS3, Wireshark, PuTTY, TFTP Server.
             </li>
             <li>
-              <span className="font-bold text-black dark:text-white mr-1">Network Services & Security:</span> DHCP, NAT/PAT, ACLs, VPN, Network Security
+              <span className="font-bold text-black dark:text-white mr-1">Programming & Automation:</span> Python, Netmiko, JavaScript, HTML, MySQL.
             </li>
             <li>
-              <span className="font-bold text-black dark:text-white mr-1">Network Tools:</span> Cisco Packet Tracer, GNS3, TFTP Server,
+              <span className="font-bold text-black dark:text-white mr-1">Familiar:</span> Docker, linux (Ubuntu, Kali Linux)
             </li>
             <li>
-              <span className="font-bold text-black dark:text-white mr-1">Programming & Automation:</span> Python, JavaScript, HTML, NodeJS, MySQL
+              <span className="font-bold text-black dark:text-white mr-1">Languages:</span> Thai (Native), English (Able to read technical documentation)
             </li>
-          
           </ul>
-        </div>
       </div>
     </section>
   );
